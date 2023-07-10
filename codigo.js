b2b6b4a8ad82a91ce18669ea20f1647f45df561c
jonatan tejeda <chonitejeda01@gmail.com>
2023-07-10 11:49:18 -0300

    Agregado de function

--- codigo.js
+++ codigo.js
@@ -1,3 +1,11 @@
+function saludar(nombre) {
+  console.log(`¡Hola, ${nombre}! Bienvenido/a a Omen Party.`);
+}
+
+const mostrarMensaje = mensaje => {
+  console.log(mensaje);
+};
+
 alert("Bienvenido a Omen Party!");
 
 let cantidadPersonas = parseInt(prompt("Cantidad de personas:"));
@@ -11,23 +19,25 @@ while (i < cantidadPersonas) {
     edad = parseInt(prompt("Edad:"));
 
     if (isNaN(edad) || edad <= 0) {
-      console.log("Edad inválida. Por favor, ingresa un número válido.");
+      mostrarMensaje("Edad inválida. Por favor, ingresa un número válido.");
     } else {
       break;
     }
   }
 
   if (edad >= 18) {
-    console.log(`${nombre}, tienes ${edad} años, eres mayor de edad, puedes pasar.`);
+    mostrarMensaje(`${nombre}, tienes ${edad} años, eres mayor de edad, puedes pasar.`);
+    saludar(nombre);
   } else if (edad >= 14 && edad <= 17) {
     let acompañado = prompt("¿Estás acompañado de un adulto? (SI/NO):");
     if (acompañado.toUpperCase() === "SI") {
-      console.log(`${nombre}, tienes ${edad} años y estás acompañado, puedes pasar.`);
+      mostrarMensaje(`${nombre}, tienes ${edad} años y estás acompañado, puedes pasar.`);
+      saludar(nombre);
     } else {
-      console.log(`${nombre}, tienes ${edad} años y necesitas estar acompañado para poder pasar.`);
+      mostrarMensaje(`${nombre}, tienes ${edad} años y necesitas estar acompañado para poder pasar.`);
     }
   } else {
-    console.log(`${nombre}, tienes ${edad} años, eres menor de edad.`);
+    mostrarMensaje(`${nombre}, tienes ${edad} años, eres menor de edad.`);
   }
 
   i++;
