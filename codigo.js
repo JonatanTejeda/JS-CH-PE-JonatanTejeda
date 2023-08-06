df2c38af5bd5e7516ab48b97e1dd9e762f5b948f
jonatan tejeda <chonitejeda01@gmail.com>
2023-08-06 14:26:59 -0300

    preentrega numero 3

--- codigo.js
+++ codigo.js
@@ -1,41 +1,97 @@
-const carrito = [];
-const preciosEntradas = {
-  general: 3000,
-  vip: 6000,
-  'vip-bebida': 13000,
-};
+document.addEventListener("DOMContentLoaded", () => {
+  const listaCarrito = document.getElementById("lista-carrito");
+  const totalCarrito = document.getElementById("total");
+  const botonFinalizarCompra = document.getElementById("finalizar-compra");
+  const botonesAgregar = document.querySelectorAll(".agregar");
+  let carrito = cargarCarritoDesdeLocalStorage();
 
-function agregarEntrada() {
-  const tipoEntrada = prompt("Ingresa el tipo de entrada (general, vip o vip-bebida):");
-  if (!tipoEntrada || !preciosEntradas[tipoEntrada]) {
-    alert("Tipo de entrada inválido. Inténtalo nuevamente.");
-    return;
+  botonesAgregar.forEach((boton) => {
+    boton.addEventListener("click", agregarProducto);
+  });
+
+  botonFinalizarCompra.addEventListener("click", confirmarFinalizarCompra);
+
+  listaCarrito.addEventListener("click", eliminarProducto);
+
+  document.getElementById("cart-button").addEventListener("click", () => {
+    const carrito = document.querySelector(".carrito");
+    carrito.classList.toggle("hidden");
+  });
+
+  function agregarProducto(e) {
+    const productoSeleccionado = e.target.closest(".producto");
+    const nombre = productoSeleccionado.querySelector("p").textContent;
+    const precio = parseFloat(productoSeleccionado.querySelector(".precio").textContent.slice(1));
+
+    const producto = carrito.find((item) => item.nombre === nombre);
+
+    if (producto) {
+      producto.cantidad++;
+    } else {
+      carrito.push({
+        nombre,
+        precio,
+        cantidad: 1
+      });
+    }
+
+    guardarCarritoEnLocalStorage();
+    actualizarCarrito();
   }
 
-  console.log(`Tipo de entrada: ${tipoEntrada}`);
-  carrito.push({ tipoEntrada, precio: preciosEntradas[tipoEntrada] });
-  console.log('Carrito actual:', carrito);
-  mostrarCarrito();
-  calcularTotal();
-}
+  function actualizarCarrito() {
+    listaCarrito.innerHTML = "";
+    let total = 0;
+    let itemCount = 0;
 
-function mostrarCarrito() {
-  const carritoUl = document.getElementById('carrito');
-  carritoUl.innerHTML = '';
-  carrito.forEach((item, index) => {
-    const li = document.createElement('li');
-    const precioTexto = item.precio === 0 ? 'Gratis' : `$${item.precio.toFixed(2)}`;
-    li.textContent = `${item.tipoEntrada}: ${precioTexto}`;
-    carritoUl.appendChild(li);
-  });
-}
+    carrito.forEach((producto, index) => {
+      const { nombre, precio, cantidad } = producto;
+      total += precio * cantidad;
+      itemCount += cantidad;
 
-function calcularTotal() {
-  const totalDiv = document.getElementById('total');
-  const total = carrito.reduce((sum, item) => sum + item.precio, 0);
-  console.log(`Total de la compra: $${total.toFixed(2)}`);
-  totalDiv.textContent = `Total: $${total.toFixed(2)}`;
-}
+      const itemCarrito = document.createElement("li");
+      itemCarrito.innerHTML = `
+        ${nombre} - Cantidad: ${cantidad} - $${(precio * cantidad).toFixed(2)}
+        <button class="eliminar">
+          <img src="img/trash.png" alt="Eliminar">
+        </button>`;
+      listaCarrito.appendChild(itemCarrito);
+    });
 
-const botonAgregar = document.getElementById('agregar');
-botonAgregar.addEventListener('click', agregarEntrada);
+    totalCarrito.textContent = total.toFixed(2);
+    document.getElementById("item-count").textContent = itemCount;
+  }
+
+  function confirmarFinalizarCompra() {
+    const confirmacion = confirm("¿Estás seguro de finalizar la compra?");
+    if (confirmacion) {
+      finalizarCompra();
+      swal("¡Gracias por tu compra!", "OMEN PARTY - BEBIDAS!", "success");
+    }
+  }
+
+  function finalizarCompra() {
+    carrito = [];
+    guardarCarritoEnLocalStorage();
+    actualizarCarrito();
+  }
+
+  function eliminarProducto(e) {
+    const botonEliminar = e.target.closest(".eliminar");
+    if (botonEliminar) {
+      const index = Array.from(listaCarrito.children).indexOf(botonEliminar.parentElement);
+      carrito.splice(index, 1);
+      guardarCarritoEnLocalStorage();
+      actualizarCarrito();
+    }
+  }
+
+  function guardarCarritoEnLocalStorage() {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }
+
+  function cargarCarritoDesdeLocalStorage() {
+    const carritoGuardado = localStorage.getItem("carrito");
+    return carritoGuardado ? JSON.parse(carritoGuardado) : [];
+  }
+});
